a4c31b143e31f0e4d679985aea3535b93999bd28
mauroerta <mauro@vlkstudio.com>
2020-09-06 12:39:45 +0200

    First pages commit

---
+++ node_modules/.cache/gh-pages/git@github.com!mauroerta!game-of-life.git/precache-manifest.fb037eadf7b18556062075de1e6475d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d1920741906496e51e324fb5fcb708f",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "fc8a0012b582580590bd",
+    "url": "/game-of-life/static/js/2.6632a2b5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/game-of-life/static/js/2.6632a2b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b47cd7aff72b23af7e1",
+    "url": "/game-of-life/static/js/main.6da250e9.chunk.js"
+  },
+  {
+    "revision": "a6893dc5f1659096e143",
+    "url": "/game-of-life/static/js/runtime-main.1dafd470.js"
+  }
+]);
